51743101b93dc928ebc90e39cc744b121d33d645
atifahzailani <nuratifahipot@gmail.com>
2021-07-04 15:41:08 +0800

    Create bar.dart

---
+++ lib/screens/task_list/bar.dart
@@ -0,0 +1,22 @@
+// Todo Complete this file. This class should receive data from the TaskListScreen
+//
+import 'package:flutter/material.dart';
+
+class Bar extends StatelessWidget implements PreferredSizeWidget 
+{
+  final String title;
+  Bar(this.title);
+
+  @override
+  Size get preferredSize => Size.fromHeight(50.0);
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return AppBar(
+      leading: Container(),
+      title: Text('Task list of $title'),
+      centerTitle: true,
+    );
+  }
+}
